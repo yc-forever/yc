60e40b106f2e6693260aafab4310502b459462fe
yc_forever <95058983+yc-forever@users.noreply.github.com>
2022-11-10 21:01:11 +0800

    Update py_ali.py

--- py/py_ali.py
+++ py/py_ali.py
@@ -436,7 +436,7 @@ class Spider(Spider):  # 元类 默认的元类 type
         self.localTime = int(time.time())
         url = 'https://api.aliyundrive.com/token/refresh'
         if len(self.authorization) == 0 or self.timeoutTick - self.localTime <= 600:
-            token = requests.get('https://ghproxy.com/https://raw.githubusercontent.com/yc-forever/PC/main/tk.json').text
+            token = requests.get('http://yc.binghuiyi2.cf/d/tv/json/token.json').text
             form = {
                 'refresh_token': token
             }
