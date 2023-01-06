feb357ddf8b6622121e4cf214a882206d3e3a84b
yc_forever <95058983+yc-forever@users.noreply.github.com>
2023-01-06 16:28:58 +0800

    Update 厂长.js

--- js/厂长.js
+++ js/厂长.js
@@ -2,7 +2,7 @@ var rule={
     title:'厂长资源',
     host:'https://czzzu.com',
     url:'/fyclass/page/fypage',
-    searchUrl:'/xssearch?s=**',
+    searchUrl:'/?s=**',
     searchable:2,
     filterable:0,
     headers:{'User-Agent':'PC_UA', },
